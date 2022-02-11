b72e7097110e2a3d22a459e0200a8c861c561e11
guchenyu <guchenyu150533@gmail.com>
2022-02-11 19:09:41 +0800

    修改了py2的importt写法

--- scripts/main.py
+++ scripts/main.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 
-import scripts.verification as ver
+from . import verification as ver
 
 
 class Check:
